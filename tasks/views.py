41f4bf5cb5662ff7bf3c78a45fa51b565926d878
gabriel.moreira <gabrielpsdm@gmail.com>
2020-03-09 17:14:52 -0300

    backend pronto django rest framework pronto integrar o React

--- tasks/views.py
+++ tasks/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
-from.forms import *
-from rest_framework import status
+from .forms import *
 
 from .serializer import TaskDataSerializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
 
 def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
 
-    context = {'tasks': tasks, 'form':form}
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
 
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
@@ -34,27 +36,33 @@ def updateTask(request, pk):
             form.save()
             return redirect('/')
 
-    context = {'form':form}
+    context = {'form': form}
 
     return render(request, 'tasks/update_task.html', context)
 
-def deleteTask (request, pk):
 
+def deleteTask(request, pk):
     item = Task.objects.get(id=pk)
 
     if request.method == 'POST':
         item.delete()
         return redirect('/')
 
-    context = {'item':item}
+    context = {'item': item}
     return render(request, 'tasks/delete.html', context)
+
 
 class TaskListView(APIView):
     serializer_class = TaskDataSerializer
 
     def get(self, request, format=None):
         serializer = self.serializer_class(Task.objects.all(), many=True)
-        return  Response(serializer.data)
+        return Response(serializer.data)
 
-    def post(self,request, format=None):
-        serializer = self.serializer_class(Task.objects.all(), many=True)
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "403 Forbidden"}, status=status.HTTP_409_CONFLICT )
