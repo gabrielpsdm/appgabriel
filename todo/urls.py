dbdbc871bda1a50fd8ce8762c464ef7800717ad3
gabriel.moreira <gabrielpsdm@gmail.com>
2020-03-11 14:47:56 -0300

    backend pronto django rest framework pronto integrar o React

--- todo/urls.py
+++ todo/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
     path('', include('tasks.urls')),
-
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls'))
 
 ]
